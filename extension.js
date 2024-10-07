020908c523051c640c87d1df12f72817e63335b5
faymaz <fethi.aymaz@gmail.com>
2024-10-07 23:13:45 +0200

    Version Update

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 import St from 'gi://St';
-import { PanelMenu, Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import GLib from 'gi://GLib';
 import Clutter from 'gi://Clutter';
 
@@ -10,19 +10,19 @@ import Clutter from 'gi://Clutter';
 
 export default class DexcomExtension extends Extension {
     enable() {
-        // Durum göstergesini PanelMenu.Button ile oluşturuyoruz
+        
         this._indicator = new PanelMenu.Button(0.0, 'Dexcom Monitor', false);
 
-        // Label ekliyoruz
+        
         let label = new St.Label({ text: 'Dexcom Monitor', y_align: Clutter.ActorAlign.CENTER });
         this._indicator.add_child(label);
 
-        // Durum göstergesini panelin sağ tarafına ekliyoruz
+        
         Main.panel.addToStatusArea(this.uuid, this._indicator);
 
-        // İlk veri güncellemesini başlatıyoruz
+        
         this._updateGlucose();
-        // Her 3 dakikada bir veriyi yeniliyoruz
+        
         this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 180, () => {
             this._updateGlucose();
             return GLib.SOURCE_CONTINUE;
@@ -84,8 +84,8 @@ export default class DexcomExtension extends Extension {
                 headers: { 'Content-Type': 'application/json' }
             });
 
-            let glucoseData = await glucoseResponse.json();  // JSON formatında verileri alıyoruz
-            return glucoseData[0];  // En son glukoz verisini döndürüyoruz
+            let glucoseData = await glucoseResponse.json();
+            return glucoseData[0];
         } catch (error) {
             logError(error);
             return null;
