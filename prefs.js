cca6b06d893c282e169422a4f35758d53b94dd3a
faymaz <fethi.aymaz@gmail.com>
2024-10-09 11:04:08 +0200

    Version Update

--- prefs.js
+++ prefs.js
@@ -1,37 +1,34 @@
-const { Gtk, Gio } = imports.gi;
-const ExtensionUtils = imports.misc.extensionUtils;
+import Gio from 'gi://Gio';
+//const Gio = imports.gi.Gio;
+//const Gtk = imports.gi.Gtk;
+import Gtk from 'gi://Gtk';
 
 function init() {}
 
 function buildPrefsWidget() {
-    let settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.dexcom');
+    let widget = new Gtk.Grid({ column_spacing: 12, row_spacing: 12, margin: 18 });
 
-    let widget = new Gtk.Grid({
-        margin_top: 24,
-        margin_bottom: 24,
-        margin_start: 24,
-        margin_end: 24,
-        column_spacing: 12,
-        row_spacing: 12,
+    let settings = new Gio.Settings({ schema_id: 'org.gnome.shell.extensions.dexcom' });
+
+    // Username input
+    let usernameLabel = new Gtk.Label({ label: "Dexcom Username", halign: Gtk.Align.START });
+    let usernameEntry = new Gtk.Entry({ text: settings.get_string('username') });
+    usernameEntry.connect('changed', (entry) => {
+        settings.set_string('username', entry.get_text());
     });
 
-    let usernameLabel = new Gtk.Label({ label: "Dexcom Username", hexpand: true, halign: Gtk.Align.START });
-    let usernameEntry = new Gtk.Entry({ text: settings.get_string('username') });
+    // Password input
+    let passwordLabel = new Gtk.Label({ label: "Dexcom Password", halign: Gtk.Align.START });
+    let passwordEntry = new Gtk.Entry({ text: settings.get_string('password') });
+    passwordEntry.set_visibility(false);  // Hide the password
+    passwordEntry.connect('changed', (entry) => {
+        settings.set_string('password', entry.get_text());
+    });
+
     widget.attach(usernameLabel, 0, 0, 1, 1);
     widget.attach(usernameEntry, 1, 0, 1, 1);
-
-    usernameEntry.connect('changed', () => {
-        settings.set_string('username', usernameEntry.get_text());
-    });
-
-    let passwordLabel = new Gtk.Label({ label: "Dexcom Password", hexpand: true, halign: Gtk.Align.START });
-    let passwordEntry = new Gtk.Entry({ text: settings.get_string('password'), visibility: false });
     widget.attach(passwordLabel, 0, 1, 1, 1);
     widget.attach(passwordEntry, 1, 1, 1, 1);
-
-    passwordEntry.connect('changed', () => {
-        settings.set_string('password', passwordEntry.get_text());
-    });
 
     return widget;
 }
